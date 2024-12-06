35f745ef9c3220aed3dede64e60a6e2d19036885
Gabriele Claudio Baroni <gabriele.baroni@usnc.it>
2024-12-07 00:39:32 +0100

    empty modal container component

---
+++ src/app/components/modal-container/modal-container.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-container',
+  standalone: false,
+  
+  templateUrl: './modal-container.component.html',
+  styleUrl: './modal-container.component.scss'
+})
+export class ModalContainerComponent {
+
+}
---
+++ src/app/components/modal-container/modal-container.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ModalContainerComponent } from './modal-container.component';
+
+@NgModule({
+  declarations: [ModalContainerComponent],
+  imports: [CommonModule],
+  exports: [ModalContainerComponent],
+})
+export class ModalContainerModule {}
