34490fda720a8436872e8cca4bd5c65a2650e21d
Gabriele Claudio Baroni <gabriele.baroni@usnc.it>
2024-12-06 23:00:04 +0100

    productsService

--- src/app/products/products-list/products-list.component.ts
+++ src/app/products/products-list/products-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { MatTableDataSource } from '@angular/material/table';
+import { ProductsService } from '../products.service';
 @Component({
   selector: 'app-products-list',
   standalone: false,
@@ -12,15 +13,15 @@ export class ProductsListComponent {
   displayedColumns: string[] = ['id', 'name', 'price', 'description'];
   dataSource = new MatTableDataSource<any>();
 
-  constructor(private http: HttpClient) {}
+  constructor(private productsService: ProductsService) {}
 
   ngOnInit(): void {
     this.loadProducts();
   }
 
   loadProducts(): void {
-    this.http.get('https://dummyjson.com/products').subscribe(
-      (response: any) => {
+    this.productsService.getProducts().subscribe(
+      (response) => {
         this.dataSource.data = response.products;
       },
       (error) => {
