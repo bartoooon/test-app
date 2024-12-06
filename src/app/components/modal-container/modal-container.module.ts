9775d61342acd39f05823f3252809be53f698599
Gabriele Claudio Baroni <gabriele.baroni@usnc.it>
2024-12-07 00:54:14 +0100

    close button on top of modal

--- src/app/components/modal-container/modal-container.module.ts
+++ src/app/components/modal-container/modal-container.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { ModalContainerComponent } from './modal-container.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatIcon } from '@angular/material/icon';
 
 @NgModule({
   declarations: [ModalContainerComponent],
-  imports: [CommonModule, MatDialogModule, MatButtonModule],
+  imports: [CommonModule, MatDialogModule, MatButtonModule, MatIcon],
   exports: [ModalContainerComponent],
 })
 export class ModalContainerModule {}
