b30b16fccdbc5f41cae7b5adb12f5d18eb8cebab
Gabriele Claudio Baroni <gabriele.baroni@usnc.it>
2024-12-06 18:26:12 +0100

    rough styling for login

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,9 +4,17 @@ import { FormsModule } from '@angular/forms';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
-  imports: [CommonModule, FormsModule, AuthRoutingModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    AuthRoutingModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
 })
 export class AuthModule {}
