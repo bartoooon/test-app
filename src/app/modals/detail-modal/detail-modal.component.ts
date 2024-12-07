27173ab7728fe2d91e463fc9262b2cf7f48ee3f2
Gabriele Claudio Baroni <gabriele.baroni@usnc.it>
2024-12-07 14:34:17 +0100

    insert works

--- src/app/modals/detail-modal/detail-modal.component.ts
+++ src/app/modals/detail-modal/detail-modal.component.ts
@@ -31,30 +31,39 @@ export class DetailModalComponent implements OnInit {
     this.isEditable = data.isEditable; // Imposta i dati iniziali
   }
 
-  ngOnInit(): void {
-    console.log(this.data);
-    console.log(this.isEditable);
-  }
+  ngOnInit(): void {}
 
   // Funzione per alternare lo stato di modifica
   toggleEdit(): void {
     if (this.isEditable) {
       // Logica per salvare le modifiche (simulazione della POST)
-      this.updateProductTitle();
+      !this.data.id ? this.addNewProduct() : this.updateProductTitle();
     }
     this.isEditable = !this.isEditable;
   }
 
   addNewProduct(): void {
-    // this.productsService.addProduct(this.newProduct).subscribe(
-    //   (response) => {
-    //     console.log('Product added successfully:', response);
-    //     // Puoi fare qualcosa con la risposta, come mostrare un messaggio di successo o reindirizzare l'utente
-    //   },
-    //   (error) => {
-    //     console.error('Error adding product:', error);
-    //   }
-    // );
+    const newProduct: any = {
+      title: this.product.title,
+      price: this.product.price,
+      category: this.product.category,
+      brand: this.product.brand,
+      rating: this.product.rating,
+      availabilityStatus: this.product.availabilityStatus,
+      tags: this.product.tags,
+    };
+    if (Object.keys(newProduct).length > 0) {
+      this.productsService.addProduct(newProduct).subscribe((response) => {
+        console.log('Product added successfully:', response);
+        this.productUpdated.emit(this.product); // Notifica al componente padre dell'aggiornamento
+        this.dialogRef.close(this.product); // Chiudi la modale dopo l'aggiornamento      },
+        (error: any) => {
+          console.error('Error adding product:', error);
+        };
+      });
+    } else {
+      console.log('No changes to update.');
+    }
   }
 
   updateProductTitle(): void {
